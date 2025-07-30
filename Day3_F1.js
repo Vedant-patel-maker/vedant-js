7a37af3fabfe69cb8dbb3603903d32da641bb4c4
Vedant Patel <vedant.patel5110@gmail.com>
2025-07-30 21:28:11 +0530

    Third day task

---
+++ Day3_F1.js
@@ -0,0 +1,8 @@
+alert("welcome to JavaScript tutorial");
+console.log("welcome to JavaScript tutorial");
+document.getElementById("para1").innerHTML = "<b>This text is became bold through innerHTML JavaScript</b>";
+document.getElementById("heading").innerText = " this is javascript heading"
+document.getElementById("para2").innerText =  "<b> this is second paragraph it is not bold using innerText</b>";
+document.getElementById("para3").innerHTML = "innerHTML can also write the text only"
+console.log(2+5,"checkline7")
+console.log("the last console of the program","checkline8")
